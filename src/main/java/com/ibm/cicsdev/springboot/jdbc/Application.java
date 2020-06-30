0a92605e9b45bf1b7e08e5b1acfba9d035a571aa
Tony Fitzgerald <tony_fitzgerald@uk.ibm.com>
2020-06-30 16:20:17 +0100

    update

--- src/main/java/com/ibm/cicsdev/springboot/jdbc/Application.java
+++ src/main/java/com/ibm/cicsdev/springboot/jdbc/Application.java
@@ -24,7 +24,6 @@ import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 
 /**
